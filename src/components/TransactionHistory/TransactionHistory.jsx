6181bc7c017d48e177aa81da80ffb69e73720041
Lia <natalimail911@gmail.com>
2024-12-07 17:29:09 +0200

    css

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,38 +3,41 @@ import styles from "./TransactionHistory.module.css";
 
 const TransactionHistory = ({ items }) => {
   console.log("Items in TransactionHistory:", items);
-    return (
-      <table className={styles.transactionHistory}> 
+  return (
+    <table className={styles.transactionHistory}>
       {/* "transaction-history" */}
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }, index) => (
+          <tr
+            key={id}
+            className={index % 2 === 0 ? styles.evenRow : styles.oddRow}
+          >
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
           </tr>
-        </thead>
-        <tbody>
-          {items.map(({ id, type, amount, currency }) => (
-            <tr key={id}>
-              <td>{type}</td>
-              <td>{amount}</td>
-              <td>{currency}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  };
-  
-  TransactionHistory.propTypes = {
-    items: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-        amount: PropTypes.string.isRequired,
-        currency: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-  };
-  
-  export default TransactionHistory;
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default TransactionHistory;
