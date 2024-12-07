cfc45564e05087a207af60996456e3a07d74ed65
Lia <natalimail911@gmail.com>
2024-12-07 03:14:47 +0200

    app

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,40 @@
+import PropTypes from "prop-types";
+import styles from "./TransactionHistory.module.css";
+
+const TransactionHistory = ({ items }) => {
+  console.log("Items in TransactionHistory:", items);
+    return (
+      <table className={styles.transactionHistory}> 
+      {/* "transaction-history" */}
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+  
+  TransactionHistory.propTypes = {
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+        currency: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+  };
+  
+  export default TransactionHistory;
